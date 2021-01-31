ef0869a7d2577cdc274f8a658fd3353085b8accb
Esaou <eric.saou3@gmail.com>
2021-01-31 16:55:16 +0100

    modification design

--- public/js/Animation.js
+++ public/js/Animation.js
@@ -1,3 +1,22 @@
 $( document ).ready(function() {
     new WOW().init();
 });
+
+let header = document.querySelector(".nav");
+let listeBtn = document.querySelector(".listeBtn");
+let checkBox = document.querySelector(".checkBox");
+let lastScrollValue = 0;
+
+document.addEventListener('scroll',() => {
+    checkBox.checked = false;
+        let top  = document.documentElement.scrollTop;
+    if(top > 75) {
+        header.classList.add("hidden");
+    } 
+    if(lastScrollValue > top) {
+        header.classList.remove("hidden");
+    }
+
+    
+    lastScrollValue = top;
+});
